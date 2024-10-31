43d6b82cf37a4805b9df723649c7a6b9a70aa0a7
TechAurelian <dev@techaurelian.com>
2024-10-31 14:45:20 +0200

    Rename app and improve code docs

--- recursive_routing/lib/common/strings.dart
+++ recursive_routing/lib/common/strings.dart
@@ -1,7 +1,12 @@
+// Copyright 2020-2024 Hellogramming. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://hellogramming.com/flutter_experiments/license/.
+
 /// Application wide UI string constants.
 library;
 
-const String appName = 'Flutter Experiments';
+const String appName = 'Flutter Recursive Routing';
 String recursiveRoutingScreenTitle(int depth) => 'Recursive Routing $depth';
 const String goHomeTitle = 'Go home';
 const String goDeeperTitle = 'Go deeper';
--- recursive_routing/lib/main.dart
+++ recursive_routing/lib/main.dart
@@ -1,22 +1,27 @@
+// Copyright 2020-2024 Hellogramming. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://hellogramming.com/flutter_experiments/license/.
+
 import 'package:flutter/material.dart';
 
 import 'common/strings.dart' as strings;
 import 'screens/recursive_routing_screen.dart';
 
 void main() {
-  runApp(const RecursiveRoutingApp());
+  runApp(const RecursiveRoutingExperiment());
 }
 
-class RecursiveRoutingApp extends StatelessWidget {
-  const RecursiveRoutingApp({super.key});
+/// The root widget of the Recursive Routing Flutter experiment.
+class RecursiveRoutingExperiment extends StatelessWidget {
+  const RecursiveRoutingExperiment({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: strings.appName,
-      theme: ThemeData(),
+      theme: ThemeData.light(),
       home: const RecursiveRoutingScreen(1),
     );
   }
